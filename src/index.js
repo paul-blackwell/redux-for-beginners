8b0c1fa893868107813afb8a72f2ceca9bee87e1
Paul Blackwell <paul@paulblackwell.io>
2021-09-11 10:04:37 +0100

    Set up basic Redux global state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+
+// STORE => GLOBALIZED STATE
+
+// ACTION
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT": 
+      return state -1;
+    default:
+      return state;
+  }
+};
+
+let store = createStore(counter);
+
+// Display in console
+store.subscribe(() => console.log(store.getState()))
+
+
+// DISPATCH
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
